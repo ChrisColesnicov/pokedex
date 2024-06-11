f72e26566edc60d2c0d06aa492470a2fb81edc1e
Christian Colesnicov <chris.colesnicov@gmail.com>
2024-06-11 16:52:34 +0200

    React Basics 03 - changed PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,20 @@
 export default function PokemonCard() {
-    return (<figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image de Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>)
+    const pokemon = pokemonList[0];
+    return (
+        <figure>
+            {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.imgSrc} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
 }
+
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "Mew",
+    },
+];
