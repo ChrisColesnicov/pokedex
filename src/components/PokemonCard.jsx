7cd3cdbd9f8a98475b8dcae201e6986ad0ae0093
Christian Colesnicov <chris.colesnicov@gmail.com>
2024-06-11 15:56:19 +0200

    React Basics 02 - added Bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,6 @@
+export default function PokemonCard() {
+    return (<figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image de Bulbasaur" />
+        <figcaption>Bulbasaur</figcaption>
+    </figure>)
+}
