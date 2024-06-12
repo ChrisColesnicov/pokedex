4509e35addbf455b3276656b14b06c1175a2e551
Christian Colesnicov <chris.colesnicov@gmail.com>
2024-06-12 15:27:00 +0200

    React basics 04 done, props used for pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,9 @@
-export default function PokemonCard() {
-    const pokemon = pokemonList[0];
+export default function PokemonCard({ pokemon }) {
     return (
         <figure>
-            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.imgSrc} /> : <p>???</p>}
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
 
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    },
-];
